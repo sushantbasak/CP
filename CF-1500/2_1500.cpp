400b442915d3da594803bb18778e63caefdd62e6
sushantbasak <sushantbasak73@gmail.com>
2021-06-11 23:33:07 +0530

    Add 1500 CF Practice

---
+++ CF-1500/2_1500.cpp
@@ -0,0 +1,61 @@
+/******************************************
+* AUTHOR : kevin_007 *
+******************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define checktime() cout<<"\nTime Elapsed : "<<fixed<<float(clock()-clk)/CLOCKS_PER_SEC<<setprecision(5)<<endl
+#define ll long long
+
+int solve(){
+
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++) cin >> arr[i];
+
+    multiset<ll> p1;    int ans = 0;    ll sum = 0;
+
+    for(int i=0;i<n;i++) {
+        sum += arr[i];
+        ans++;
+
+        p1.insert(arr[i]);
+
+        while(sum < 0) {
+            auto itr = p1.begin();  
+
+            sum = sum - *itr;   ans--;
+            p1.erase(itr);
+        }
+
+        // cout << sum << " " << ans << "\n";
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE   
+    freopen("input.txt", "r", stdin); 
+    // freopen("output.txt", "w", stdout);    
+    #endif 
+    Speed_UP
+
+
+    int t = 1;
+    // cin>>t;
+
+    while(t--){
+        solve();
+    }
+
+    //checktime();
+
+    return 0;
+
+}
